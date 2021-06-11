b23519f5ebadc2084013c97642333e88cb9f6d51
SumitNawathe <50125892+SumitNawathe@users.noreply.github.com>
2021-06-11 12:08:06 -0400

    Refactored frontend buttons.

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -3,10 +3,8 @@ const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true }
 
 const $participantList = document.querySelector('#participant-list');
 const participantTemplate = document.querySelector('#participant-template').innerHTML;
-const $lobbyAction = document.querySelector('#action');
-const lobbyActionStartTemplate = document.querySelector('#action-start').innerHTML;
-const lobbyActionPlayTemplate = document.querySelector('#action-play').innerHTML;
-const lobbyActionSpectateTemplate = document.querySelector('#action-spectate').innerHTML;
+const $lobbyActions = document.querySelector('#actions');
+const actionButtonTemplate = document.querySelector('#action-button-template').innerHTML;
 
 const TYPE_SPECTATOR = -1;
 const TYPE_HOST = 0;
@@ -54,43 +52,49 @@ socket.on('lobbyData', (lobbyDataString) => {
         $participantList.insertAdjacentHTML('beforeend', html);
     });
 
-    //remove and replace button
-    const currentButton = $lobbyAction.querySelector('button');
-    if (currentButton) { console.log('removing button'); currentButton.remove(); }
-    let type = -1;
-    lobbyData.users.every((person) => {
-        if (person.username === username) {
-            type = person.type;
-            return false;
-        }
-        return true;
-    });
-    let html = null;
-    if (type === TYPE_HOST) {
-        html = Mustache.render(lobbyActionStartTemplate);
-    } else if (type === TYPE_PLAYER) {
-        html = Mustache.render(lobbyActionSpectateTemplate);
-    } else { //type === TYPE_SPECTATOR
-        html = Mustache.render(lobbyActionPlayTemplate);
+    //if in lobby state, remove and replace buttons
+    if (lobbyData.gameState === GAMESTATE_LOBBY) {
+        const currentButton = $lobbyActions.querySelector('button');
+        if (currentButton) { console.log('removing button'); currentButton.remove(); }
+        let type = -1;
+        lobbyData.users.every((person) => {
+            if (person.username === username) {
+                type = person.type;
+                return false;
+            }
+            return true;
+        });
+        createLobbyButtons(type);
     }
-    $lobbyAction.insertAdjacentHTML('beforeend', html);
-    const newButton = $lobbyAction.querySelector('button');
-    if (type === TYPE_HOST) {
+});
+
+const createLobbyButtons = (playerType) => {
+    let html = null, newButton = null;
+    if (playerType === TYPE_HOST) {
+        html = Mustache.render(actionButtonTemplate, { text: 'Start Game' });
+        $lobbyActions.insertAdjacentHTML('beforeend', html);
+        newButton = $lobbyActions.querySelector('button');
         newButton.addEventListener('click', () => {
             console.log('START GAME');
         });
-    } else if (type === TYPE_PLAYER) {
+    } else if (playerType === TYPE_PLAYER) {
+        html = Mustache.render(actionButtonTemplate, { text: 'Spectate' });
+        $lobbyActions.insertAdjacentHTML('beforeend', html);
+        newButton = $lobbyActions.querySelector('button');
         newButton.addEventListener('click', () => {
             console.log('should change to spectate');
             socket.emit('changeLobbyInfo', { username, room, newType: TYPE_SPECTATOR }, (error) => { if (error) { console.log('error'); } })
         });
-    } else { //type === TYPE_SPECTATOR
+    } else { //playerType === TYPE_SPECTATOR
+        html = Mustache.render(actionButtonTemplate, { text: 'Play Game' });
+        $lobbyActions.insertAdjacentHTML('beforeend', html);
+        newButton = $lobbyActions.querySelector('button');
         newButton.addEventListener('click', () => {
             console.log('should change to play');
             socket.emit('changeLobbyInfo', { username, room, newType: TYPE_PLAYER }, (error) => { if (error) { console.log('error'); } })
         });
     }
-});
+}
 
 socket.emit('join', { username, room }, (error) => {
     if (error) {
